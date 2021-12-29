41f6574559e017a08a794552e992f9d1103bf803
tuantdn <tuan.tran@dac-datatech.vn>
2021-12-29 09:12:39 +0700

    first commit

---
+++ src/test/java/fs/mok/store/store/book/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fs.mok.store.store.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
